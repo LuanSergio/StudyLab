3114d4261f8d31e676dea3e6be1bdc377c8fa8a5
Luan <luansergiomattos@gmail.com>
2021-05-15 21:24:36 -0300

    feat: added infinite-scroll

---
+++ infinite-scroll/src/InfiniteScroll.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from "react";
+import "./InfiniteScroll.css";
+
+interface IPostList {
+  list: Array<string>;
+}
+
+export const InfiniteScroll = () => {
+  const [postList, setPostList] = useState<IPostList>({
+    list: ["1", "2", "3", "4"],
+  });
+
+  // Tracking on which page we currently are
+  const [page, setPage] = useState(1);
+  // Add loader reference
+  const loader = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    var options = {
+      root: null, // The element that is used as the viewport.
+      rootMargin: "20px", // Grow or shrink root element's bounding box
+      threshold: 1.0, // Percentage of the target's visibility the observer's callback should be executed
+    };
+    // Initialize IntersectionObserver and attach
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (loader?.current) {
+      observer.observe(loader.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Simulate adding new posts to Post List
+    setTimeout(() => {
+      const newList = ["A", "B", "C", "D"];
+
+      setPostList((currentList: IPostList) => ({
+        list: currentList.list.concat(newList),
+      }));
+    }, 500);
+  }, [page]);
+
+  // Handle what happens when user scrolls to Loading
+  const handleObserver = (entities: IntersectionObserverEntry[]) => {
+    const target = entities[0];
+    if (target.isIntersecting) {
+      setPage((page) => page + 1);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="post-list">
+        {postList.list.map((post: string, index: number) => {
+          return (
+            <div key={index} className="post">
+              <h2> {post} </h2>
+            </div>
+          );
+        })}
+        <div className="loading" ref={loader}>
+          <h2>Loading...</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ infinite-scroll/src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { InfiniteScroll } from "./InfiniteScroll";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <InfiniteScroll />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
