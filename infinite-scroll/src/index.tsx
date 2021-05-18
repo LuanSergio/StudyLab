3fd7e034cf9cbfb35496f6910ecef171dd06db31
Luan <luansergiomattos@gmail.com>
2021-05-18 00:56:20 -0300

    refactor[infinite-scroll]: fixed component folder typo

--- infinite-scroll/src/index.tsx
+++ infinite-scroll/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import InfiniteScroll from "./components/InfiniteSroll";
+import InfiniteScroll from "./components/InfiniteScroll";
 
 ReactDOM.render(
   <React.StrictMode>
