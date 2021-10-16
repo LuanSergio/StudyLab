c2e3c29dbaa7998ee4a95f8753fcdd5b46e7aafb
Luan <luan.sergio@novahaus.com.br>
2021-10-16 15:49:53 -0300

    chore[carousel]: initial mouse interactions

--- carousel/src/components/Carousel/index.tsx
+++ carousel/src/components/Carousel/index.tsx
@@ -11,28 +11,52 @@ const Carousel = ({
   tagName = "div",
 }: CarouselProps): JSX.Element => {
   const Tag = tagName as keyof JSX.IntrinsicElements;
+  const itemWidth = 689;
   const [navigationIndex, setNavigationIndex] = useState(0);
+  const [mousePosition, setMousePosition] = useState(0);
 
   function updateIndex(newIndex: number): void {
     setNavigationIndex((previousState) => previousState + newIndex);
   }
 
   function goToPreviousIndex(newIndex: number): void {
-    if (navigationIndex + 689 >= 689) {
+    if (navigationIndex + itemWidth >= itemWidth) {
       newIndex = 0;
     }
     updateIndex(newIndex);
   }
 
   function goToNextIndex(newIndex: number): void {
-    if (-(Children.count(children) * 689) >= navigationIndex - 689) {
+    if (
+      -(Children.count(children) * itemWidth) >=
+      navigationIndex - itemWidth
+    ) {
       newIndex = 0;
     }
     updateIndex(newIndex);
   }
 
+  function move(event: React.MouseEvent) {
+    event.preventDefault();
+    console.log("initial position", event.clientX);
+    updateIndex(-event.clientX);
+    setMousePosition(event.clientX);
+  }
+
+  function lock(event: React.MouseEvent) {
+    event.preventDefault();
+    console.log("end position", event.clientX);
+    const calculatedPosition = event.clientX - mousePosition;
+    console.log("calculated position", calculatedPosition);
+    if (calculatedPosition >= itemWidth / 3) {
+      goToPreviousIndex(itemWidth);
+    } else if (calculatedPosition <= -(itemWidth / 3)) {
+      goToNextIndex(-itemWidth);
+    }
+  }
+
   return (
-    <div className="carousel">
+    <div className="carousel" onMouseDown={move} onMouseUp={lock}>
       <Tag
         className="carousel__items-holder"
         style={{ transform: `translateX(${navigationIndex}px)` }}
@@ -40,8 +64,10 @@ const Carousel = ({
         {children}
       </Tag>
       <div className="carousel__controls">
-        <button onClick={() => goToPreviousIndex(689)}>{"< Previous"}</button>
-        <button onClick={() => goToNextIndex(-689)}>{"Next >"}</button>
+        <button onClick={() => goToPreviousIndex(itemWidth)}>
+          {"< Previous"}
+        </button>
+        <button onClick={() => goToNextIndex(-itemWidth)}>{"Next >"}</button>
       </div>
     </div>
   );
