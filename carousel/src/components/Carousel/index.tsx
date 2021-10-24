27714b4639da1192c581e24bef7b62a9fb7014d7
Luan <luan.sergio@novahaus.com.br>
2021-10-24 13:52:06 -0300

    feat[carousel]: add mouse events

--- carousel/src/components/Carousel/index.tsx
+++ carousel/src/components/Carousel/index.tsx
@@ -1,4 +1,4 @@
-import { Children, ReactNode, useState } from "react";
+import { Children, ReactNode, useEffect, useState } from "react";
 import "./styles.css";
 
 interface CarouselProps {
@@ -11,67 +11,34 @@ const Carousel = ({
   tagName = "div",
 }: CarouselProps): JSX.Element => {
   const Tag = tagName as keyof JSX.IntrinsicElements;
-  const itemWidth = 468.667;
-  const [navigationIndex, setNavigationIndex] = useState(0);
 
-  const [itemIndex, setItemIndex] = useState(0);
+  const [isMouseLocked, setIsMouseLocked] = useState(false);
   const [position, setPosition] = useState(0);
   const [initialPosition, setInicialPosition] = useState(0);
-  const [isMouseLocked, setIsMouseLocked] = useState(false);
-
-  function updateIndex(newIndex: number): void {
-    setNavigationIndex((previousState) => previousState + newIndex);
-  }
-
-  function goToPreviousIndex(newIndex: number): void {
-    if (navigationIndex + itemWidth >= itemWidth) {
-      newIndex = 0;
-    }
-    updateIndex(newIndex);
-  }
-
-  function goToNextIndex(newIndex: number): void {
-    if (
-      -(Children.count(children) * itemWidth) >=
-      navigationIndex - itemWidth
-    ) {
-      newIndex = 0;
-    }
-    updateIndex(newIndex);
-  }
-
-  function moveCarousel(event: React.MouseEvent): void {
-    if (isMouseLocked) {
-      const moved = initialPosition - event.clientX;
-      setPosition(moved);
-      console.log("moved", moved);
-    }
-  }
+  const [lastPosition, setLastPosition] = useState(0);
 
   function handleMouseDown(event: React.MouseEvent): void {
     event.preventDefault();
     setIsMouseLocked(true);
     setInicialPosition(event.clientX);
-    console.log(event.clientX);
   }
 
   function handleMouseMove(event: React.MouseEvent): void {
     if (isMouseLocked) {
       const travelDistance = event.clientX - initialPosition;
-      event.preventDefault();
-      // if (travelDistance > itemWidth) {
-      //   setItemIndex((previoustState) => previoustState + 1);
-      // } else if (travelDistance < -itemWidth) {
-      //   setItemIndex((previoustState) => previoustState - 1);
-      // }
-      setPosition(travelDistance);
+      setPosition(travelDistance + lastPosition);
     }
   }
 
   function handleMouseUp(event: React.MouseEvent): void {
     event.preventDefault();
+    setLastPosition(position);
     setIsMouseLocked(false);
   }
+
+  // function handleMouseLeave(event: React.MouseEvent): void {
+  //   setIsMouseLocked(false);
+  // }
 
   return (
     <div
@@ -79,19 +46,14 @@ const Carousel = ({
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
+      // onMouseLeave={handleMouseLeave}
     >
       <Tag
         className="carousel__items-holder"
-        style={{ transform: `translateX(${position}px)` }}
+        style={{ transform: `translateX(${position * 2}px)` }}
       >
         {children}
       </Tag>
-      <div className="carousel__controls">
-        <button onClick={() => goToPreviousIndex(itemWidth)}>
-          {"< Previous"}
-        </button>
-        <button onClick={() => goToNextIndex(-itemWidth)}>{"Next >"}</button>
-      </div>
     </div>
   );
 };
