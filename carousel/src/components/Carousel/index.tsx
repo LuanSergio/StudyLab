945ebfa7be3687e5f7f847cc5fb66dfdd401c002
Luan <luan.sergio@novahaus.com.br>
2021-10-30 18:40:53 -0300

    chore[carousel]: work on carousel

--- carousel/src/components/Carousel/index.tsx
+++ carousel/src/components/Carousel/index.tsx
@@ -10,18 +10,29 @@ let isMouseLocked = false;
 let initialPosition = 0;
 let lastPosition = 0;
 let position = 0;
+let index = 0;
+
+const itemWidth = 500;
+const gap = 30;
+const totalItemWidth = itemWidth + gap;
 
 const Carousel = ({
   children,
   tagName = "div",
 }: CarouselProps): JSX.Element => {
   const Tag = tagName as keyof JSX.IntrinsicElements;
+  let itemLength = Children.count(children);
 
-  function updateCarouselPosition() {
+  function updateCarouselPosition(newPosition: number): void {
     document.documentElement.style.setProperty(
       "--carousel-position",
-      `${position}px`
+      `${newPosition}px`
     );
+  }
+
+  function updateCarouselIndex(newPosition: number): void {
+    document.documentElement.style.setProperty("transition-duration", `300ms`);
+    updateCarouselPosition(newPosition);
   }
 
   function handleMouseDown(event: React.MouseEvent): void {
@@ -34,7 +45,7 @@ const Carousel = ({
     if (isMouseLocked) {
       const travelDistance = event.clientX - initialPosition;
       position = travelDistance + lastPosition;
-      updateCarouselPosition();
+      updateCarouselPosition(position);
     }
   }
 
@@ -42,21 +53,53 @@ const Carousel = ({
     event.preventDefault();
     lastPosition = position;
     isMouseLocked = false;
+    // index = Math.floor(Math.abs(position) / (totalItemWidth / 2));
+    // console.log("index", index * totalItemWidth);
+    // position = index * totalItemWidth;
+    // console.log("position", position);
+    // updateCarouselIndex(-position);
+    // console.log("updated value", index * itemWidth);
+    // console.log(
+    //   "position: ",
+    //   Math.abs(position),
+    //   "itemWidth: ",
+    //   itemWidth,
+    //   "result: ",
+    //   Math.floor(Math.abs(position) / itemWidth)
+    // );
   }
 
-  // function handleMouseLeave(event: React.MouseEvent): void {
-  //   setIsMouseLocked(false);
-  // }
+  function goToPreviousIndex(newIndex: number): void {}
+
+  function goToNextIndex(newIndex: number): void {}
+
+  function handleMouseLeave(event: React.MouseEvent): void {
+    isMouseLocked = false;
+  }
 
   return (
     <div
-      className="carousel"
+      className="container"
+      id="carousel"
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
-      // onMouseLeave={handleMouseLeave}
+      onMouseLeave={handleMouseLeave}
     >
-      <Tag className="carousel__items-holder">{children}</Tag>
+      <div
+        className="carousel"
+
+        // onMouseLeave={handleMouseLeave}
+      >
+        <Tag className="carousel__items-holder">{children}</Tag>
+      </div>
+      <div className="carousel__controls">
+        <button onClick={() => goToPreviousIndex(itemWidth)}>
+          {"< Previous"}
+        </button>
+
+        <button onClick={() => goToNextIndex(-itemWidth)}>{"Next >"}</button>
+      </div>
     </div>
   );
 };
