485d6cb1f9d119c9bc3012cfb2c5066814697afc
Luan <luan.sergio@novahaus.com.br>
2021-12-11 03:39:42 -0300

    chore[carousel]: move carousel logic to custom react hook

--- carousel/src/components/Carousel/index.tsx
+++ carousel/src/components/Carousel/index.tsx
@@ -1,21 +1,15 @@
 import { Children, ReactNode, useEffect, useRef } from "react";
 import "./styles.css";
+import useCarousel from "./hook";
 
 interface CarouselProps {
   children: ReactNode;
   tagName?: keyof JSX.IntrinsicElements;
 }
 
-let isMouseLocked = false;
-let initialPosition = 0;
-let lastPosition = 0;
-let position = 0;
-let index = 0;
-
 const itemWidth = 500;
 const gap = 30;
 const totalItemWidth = itemWidth + gap;
-const animationDuration = 300;
 
 const Carousel = ({
   children,
@@ -24,91 +18,19 @@ const Carousel = ({
   let itemsQuantity = Children.count(children);
   const Tag = tagName as keyof JSX.IntrinsicElements;
   const carouselRef = useRef<HTMLDivElement>(null);
-  const maxIndex = itemsQuantity - 1;
-  const minIndex = 0;
-  const maxPosition = (itemsQuantity - 1) * totalItemWidth;
-  const minPosition = 0;
+
+  const {
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+    handleMouseLeave,
+    goToPreviousIndex,
+    goToNextIndex,
+  } = useCarousel({ carouselRef, totalItemWidth, itemsQuantity });
 
   useEffect(() => {
     carouselRef.current?.style.setProperty("--carousel-gap", `${gap}px`);
   }, []);
-
-  async function updateCarouselPosition(newPosition: number): Promise<void> {
-    await carouselRef.current?.style.setProperty(
-      "--carousel-position",
-      `${newPosition}px`
-    );
-    index = Math.abs(Math.round(newPosition / totalItemWidth));
-  }
-
-  function setCarouselAnimationDuration() {
-    carouselRef.current?.style.setProperty(
-      "transition-duration",
-      `${animationDuration}ms`
-    );
-  }
-
-  function removeCarouselAnimationDuration() {
-    setTimeout(() => {
-      carouselRef.current?.style.setProperty("transition-duration", `0ms`);
-    }, animationDuration);
-  }
-
-  async function updateCarouselIndex(newPosition: number): Promise<void> {
-    setCarouselAnimationDuration();
-    await updateCarouselPosition(newPosition);
-    removeCarouselAnimationDuration();
-  }
-
-  function handleMouseDown(event: React.MouseEvent): void {
-    event.preventDefault();
-    isMouseLocked = true;
-    initialPosition = event.clientX;
-  }
-
-  function handleMouseMove(event: React.MouseEvent): void {
-    if (isMouseLocked) {
-      const travelDistance = event.clientX - initialPosition;
-
-      const newPosition = travelDistance + lastPosition;
-
-      if (
-        newPosition >= -Math.abs(maxPosition) &&
-        newPosition <= -Math.abs(minPosition)
-      ) {
-        position = newPosition;
-        updateCarouselPosition(position);
-      }
-    }
-  }
-
-  function handleMouseUp(event: React.MouseEvent): void {
-    event.preventDefault();
-    lastPosition = position;
-    isMouseLocked = false;
-    updateCarouselIndex(-(index * totalItemWidth));
-    lastPosition = -(index * totalItemWidth);
-  }
-
-  function handleMouseLeave(event: React.MouseEvent): void {
-    isMouseLocked = false;
-  }
-
-  function goToPreviousIndex() {
-    if (index > minIndex) {
-      index = index - 1;
-      updateCarouselIndex(-(index * totalItemWidth));
-      lastPosition = -(index * totalItemWidth);
-    }
-  }
-
-  function goToNextIndex() {
-    if (index < maxIndex) {
-      index = index + 1;
-      updateCarouselIndex(-(index * totalItemWidth));
-      lastPosition = -(index * totalItemWidth);
-    }
-  }
 
   return (
     <div
